99b8be10237a489091b65896789658697a184c4f
Yatrikoxi <tomas_novak@centrum.cz>
2023-06-20 10:37:33 +0200

    s

--- First_React/src/App.jsx
+++ First_React/src/App.jsx
@@ -1,25 +1,59 @@
+import { useEffect, useState } from "react"
+import { TodoList } from "./TodoList"
+import { NewTodoForm } from "./newTodoForm"
 import "./styles.css"
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue === null) return []
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+  
+  function handleSubmit(e) {
+    e.preventDefault()
+    setTodos(currentTodos => {
+      return [
+      ...currentTodos,
+      {id:crypto.randomUUID(), title: newItem, completed:false},
+    ]
+  })
+  setNewItem("")
+  }
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+      ...currentTodos,
+      {id:crypto.randomUUID(), title, completed:false},
+    ]
+  })
+  }
+
   return (
     <>
-    <form className="new-item-form ">
-    <div className="form-row">
-      <label htmlFor="">New Item</label>
-      <input type="text" id="item"/>
-    </div>
-    <button className="btn">Add</button>
-  </form>
+    <NewTodoForm onSubmit={addTodo}/>
   <h1 className="header">Todo list</h1>
-  <ul className="list">
-    <li>
-      <label >
-        <input type="checkbox" />
-        item 1
-      </label>
-      <button className="btn btn-danger">DELETE</button>
-    </li>
-  </ul>
+    <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
   </>
   )
 }
